565102ebf98ce8f8e1b9603d71878c53e5859f62
lsm1022 <luis.morel@unh.edu>
2025-09-16 15:08:52 -0400

    updated and moved files

--- home_script.js
+++ home_script.js
@@ -1,33 +1,8 @@
 
-
-// function closeImageViewer() {
-//     const imageViewer = document.getElementById('image_viewer_window');
-//     imageViewer.style.display = 'none';
-// }
-
-// function openImageViewer(imageSrc) {
-//     document.querySelector('.image_viewer').src = imageSrc;
-//     document.getElementById('image_viewer_window').style.display = 'block';
-
-// }
-
-// window.addEventListener('keydown', function (event) {
-
-//     // Example: Close modal with Escape key
-//     if (event.key === 'Escape') {
-//         if (document.getElementById('image_viewer_window').style.display === 'block') {
-//             document.getElementById('image_viewer_window').style.display = 'none';
-//         }
-//     }
-// });
-
+// not the most efficient algorithm but it's fine for this small list of items
 function makeMenu(menuItems, menuContainerId, type) {
-    const menuContainer = document.getElementById(menuContainerId);
-    // menuContainer.className = "tempContainer";
-    // menuContainer.innerHTML = ''; // Clear existing content
-    let count = 0;
-
-    
+  const menuContainer = document.getElementById(menuContainerId);
+  let count = 0;
     
   const frag = document.createDocumentFragment();
 
@@ -35,113 +10,161 @@ function makeMenu(menuItems, menuContainerId, type) {
 
         count++;
 
-        if (item.type == type) { // Filter for Mains only
+        if (item.type_of_food == type) { // Filter for specific item type
 
             const menuItem = document.createElement('div');
             menuItem.className = 'menu-item';
 
             const img = document.createElement('img');
-            img.src = item.img;
-            img.alt = item.name;
-            img.addEventListener('click', () => openImageViewer(item.img));
+            img.src = item.image_link;
+            img.alt = item.name_english;
+            img.addEventListener('click', () => openImageViewer(item.image_link));
 
             const content = document.createElement('div');
             content.className = 'menu-item-content';
-            content.innerHTML = `<h3>${item.name}</h3><p>${item.desc}</p>`;
+            content.innerHTML = `<h3>${item.name_english} <a>(${item.name_spanish})</a> </h3><p>${item.description_english}</p> `;
 
-            const checkboxdiv = document.createElement('div');
-            checkboxdiv.className = 'checkboxDiv';
-            checkboxdiv.id = `number${count}`;
+            const pan_button_div = document.createElement('div');
+            pan_button_div.className="pan_button_div";
 
-            //minus button
-            const minusSpan = document.createElement('span');
-            minusSpan.className = 'checkboxMinus';
-            minusSpan.textContent = '-';
+           const full_pan_button = document.createElement('button');
+           full_pan_button.className = 'portionButton';
+           full_pan_button.classList.add('active');
+           full_pan_button.innerHTML = 'FULL PAN - $' +`${item.full_pan_cost}`;
 
-            //input
-            const input = document.createElement('input');
-            input.className = 'checkboxInput';
-            input.type = 'text';
-            input.value = 0;
-            input.readOnly = true;
+           
+           const half_pan_button = document.createElement('button');
+           half_pan_button.className = 'portionButton';
+           half_pan_button.innerHTML = 'HALF PAN - $' +`${item.half_pan_cost}`;
+           
+           const add_to_cart_button = document.createElement('button');
+           add_to_cart_button.className = 'addtocartButton';
+           add_to_cart_button.innerHTML = 'ADD TO CART';
 
-            //plus button
-            const plusSpan = document.createElement('span');
-            plusSpan.className = 'checkboxPlus';
-            plusSpan.textContent = '+';
 
-            const clear_text = document.createElement('p');
-            clear_text.className = 'clearButton';
-            clear_text.innerHTML = 'clear';
-            clear_text.style.display='none';
+            full_pan_button.addEventListener('click', () => {
+              if(half_pan_button.classList.contains('active')){
+                half_pan_button.classList.remove('active');
+                full_pan_button.classList.add('active');
+              }
 
-            checkboxdiv.appendChild(minusSpan);
-            checkboxdiv.appendChild(input);
-            checkboxdiv.appendChild(plusSpan);
-            checkboxdiv.appendChild(clear_text);
+               if(cart.find(i => i.id === item.id) === undefined && add_to_cart_button.classList.contains('disableAddToCart')){
+                    add_to_cart_button.classList.remove('disableAddToCart')
+               }
+               else if(cart.find(i => i.id === item.id) && add_to_cart_button.classList.contains('disableAddToCart') == false){
+                    add_to_cart_button.classList.add('disableAddToCart')
+               }
 
-            clear_text.addEventListener('click', () => {
-                clear_text.style.display='none';
-                numOfOrders -= input.value;
-                input.value = 0;
-                let cartNum = document.getElementById('cart_num_id');
-                 if(cartNum.classList.contains("active") && numOfOrders == 0){
-                    cartNum.classList.remove("active");
-                  }
-                  cartNum.innerHTML = numOfOrders;
+            });
+
+             half_pan_button.addEventListener('click', () => {
+              if(full_pan_button.classList.contains('active')){
+                full_pan_button.classList.remove('active');
+                half_pan_button.classList.add('active');
+              }
+
+
+                if(cart.find(i => i.id === (item.id + 10000)) === undefined && add_to_cart_button.classList.contains('disableAddToCart')){  
+  
+                  add_to_cart_button.classList.remove('disableAddToCart')
+               }
+               else if(cart.find(i => i.id === (item.id + 10000)) && add_to_cart_button.classList.contains('disableAddToCart') == false){
+                    add_to_cart_button.classList.add('disableAddToCart')
+               }
+                
             });
 
 
-            //click event listeners
-            plusSpan.addEventListener('click', () => {
-                // input.value = (parseInt(input.value) === 3) ? 3 : parseInt(input.value) + 1;
-                if(parseInt(input.value) === 3){
-                  input.value = 3;
-                }else{
-                  input.value =parseInt(input.value) + 1;
-                  numOfOrders += 1;
-                   //add active class
-                  let cartNum = document.getElementById('cart_num_id');
-                  if(!cartNum.classList.contains("active")){
-                    cartNum.classList.add("active");
-                  }
-                    cartNum.innerHTML= numOfOrders;
-                 
-                }
+           pan_button_div.appendChild(full_pan_button);
+           pan_button_div.appendChild(half_pan_button);
 
-                  if(parseInt(input.value) !== 0)
-                    clear_text.style.display='block';
+          // //  pan_button_div.appendChild(checkboxdiv);
+          //  pan_button_div.appendChild(checkboxdiv);
+           
+           
+           content.appendChild(pan_button_div);
+           content.appendChild(add_to_cart_button);
+             
+           // --------------
+
+
+           add_to_cart_button.addEventListener('click', ()=>{
+
+
+                if(full_pan_button.classList.contains('active')){
+
+            
+
+                  if(add_to_cart_button.classList.contains('disableAddToCart') == false){
+
+                    if(addToCart(item.id, item.name_english, item.image_link, 1, item.full_pan_cost, add_to_cart_button, full_pan_button, half_pan_button) == true){
+                      
+                
+
+                      add_to_cart_button.classList.add('disableAddToCart');
+                    }
+                  }
+
                   else
-                    clear_text.style.display='none';
-            });
+                    return;
 
-            minusSpan.addEventListener('click', () => {
-                // input.value = (parseInt(input.value) === 0) ? 0 : parseInt(input.value) - 1;
-                 if(parseInt(input.value) === 0){
-                  input.value = 0;
-                }else{
-                  input.value = parseInt(input.value) - 1;
-                  numOfOrders -= 1;
-                  //remove? active class
-                    let cartNum = document.getElementById('cart_num_id');
-                  if(numOfOrders == 0 && cartNum.classList.contains("active")){
-                    cartNum.classList.remove("active");
-                  }
-                    cartNum.innerHTML= numOfOrders;
                 }
+                else if(half_pan_button.classList.contains('active')){
 
-                if(parseInt(input.value) !== 0)
-                    clear_text.style.display='block';
+                
+
+                  if(add_to_cart_button.classList.contains('disableAddToCart') == false){
+              
+
+                    if(addToCart(item.id, item.name_english, item.image_link, 2, item.half_pan_cost, add_to_cart_button, full_pan_button, half_pan_button) == true){
+                
+                      add_to_cart_button.classList.add('disableAddToCart');
+                    }
+                  }
                   else
-                    clear_text.style.display='none';
-            });
+                    return;
+
+                }
+                else{
+                  console.log("Error - no active class for pans for some reason");
+                  return;
+                }
+                
+                     document.getElementById('cart_num_id').style.display = 'block';
+                     document.getElementById('cart_num_id').innerHTML = cart.length;
+                     renderCart(); //maybe do on cart open
+
+           });
+
 
             menuItem.appendChild(img);
-            content.appendChild(checkboxdiv);
             menuItem.appendChild(content);
             frag.appendChild(menuItem);
+            
         }
     });
 
     menuContainer.appendChild(frag);
+}
+
+
+function getJsonData() {
+    fetch('items.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            makeMenu(data, "menu-arroz", "rice");
+            makeMenu(data, "menu-carne", "meat");
+            makeMenu(data, "menu-pasta", "pasta");
+            makeMenu(data, "menu-viveres", "root_vegetables");
+            makeMenu(data, "menu-picadera", "finger_food");
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        })
+
 }
